057e32435088d30cd76b18ec9898b61337172d20
Student <example@example.com>
2020-01-16 10:25:47 -0800

    1. more branching practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+    var height = parseInt(prompt("Enter your height in inches:"));
+  
+    if (height >= 60) {
+      $('#superman').show();
+    } else if (height > 55 && height < 60){
+      $('#desert-storm').show();
+    } else {
+      $('#fun-times').show();       
+    }
 
 });
