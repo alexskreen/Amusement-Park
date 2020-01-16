53208afe5b9a856d36e6f34e8a25904831105cc7
Student <example@example.com>
2020-01-16 10:40:02 -0800

    1. add classes to the scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,20 @@ $(document).ready(function(){
     var height = parseInt(prompt("Enter your height in inches:"));
   
     if (height >= 60) {
-      $('#superman').show();
+      $("#superman").addClass("highlight");
+      $("#desert-storm").addClass("highlight-red");
+      $("#fun-times").addClass("highlight-red");
+      // $('#superman').show();
     } else if (height > 55 && height < 60){
-      $('#desert-storm').show();
+      // $('#desert-storm').show();
+      $("#desert-storm").addClass("highlight");
+      $("#superman").addClass("highlight-red");
+      $("#fun-times").addClass("highlight-red");
     } else {
-      $('#fun-times').show();       
-    }
+      $("#fun-times").addClass("highlight");
+      $("#desert-storm").addClass("highlight-red");
+      $("#superman").addClass("highlight-red");
+      // $('#fun-times').show();       
+    } 
 
 });
